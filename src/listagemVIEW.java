7a3e6612491003c61a1b825ff3c489509a08f469
Lorranye-Silva <lorranyevictoria05@gmail.com>
2025-04-03 22:39:47 -0300

    Funcionalidade de vender produto adicionada ao botão Vender na tela listagemVIEW

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -146,6 +146,7 @@ public class listagemVIEW extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(null, "ID inválido! Insira um número.");
     }
 
+
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
