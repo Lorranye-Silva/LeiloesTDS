11caf8abe53e0514fd6dd64386e1c6ca45796ff6
Lorranye-Silva <lorranyevictoria05@gmail.com>
2025-04-03 21:21:03 -0300

    Implementada funcionalidade de cadastro de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,11 +23,26 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+       conn = new conectaDAO().connectDB();
+    try {
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+    } finally {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+        }
+    }
+
+
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
